a306b065dba36d5a979991039a3051c44b82df27
Abbas BinVakas <abbas.bin_vakas@kcl.ac.uk>
2022-10-10 19:19:23 +0100

    Exercise 1 attempt

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,10 @@
 """Forms of the project."""
+from enum import unique
+from django import forms
 
 # Create your forms here.
+
+class ThingForm(forms.Form):
+    name = forms.CharField(max_length=35, required=False)
+    description = forms.Textarea(max_length=120, required=False)
+    quantity = forms.NumberInput(attrs={'min':0, 'max': 50})
