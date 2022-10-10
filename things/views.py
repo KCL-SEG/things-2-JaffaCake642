569b98ebd382aa7386760a2b1b2318f4dabaf9ea
Abbas BinVakas <abbas.bin_vakas@kcl.ac.uk>
2022-10-10 19:36:34 +0100

    Exercise 2 attempt

--- things/views.py
+++ things/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .forms import ThingForm
 
 def home(request):
-    form = ThingForm
+    form = ThingForm()
     return render(request, 'home.html', {'form': form})
