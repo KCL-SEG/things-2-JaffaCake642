db99e21024f3daf7db6d2c88e8e5c474d55d8787
Abbas BinVakas <abbas.bin_vakas@kcl.ac.uk>
2022-10-10 19:51:37 +0100

    Exercise 2 attempt

--- things/forms.py
+++ things/forms.py
@@ -1,15 +1,14 @@
 """Forms of the project."""
-from enum import unique
 from django import forms
 from .models import Thing
 
 # Create your forms here.
 
 class ThingForm(forms.ModelForm):
-    # name = forms.CharField(max_length=35, required=False)
-    # description = forms.Textarea(max_length=120, required=False)
-    # quantity = forms.NumberInput(attrs={'min':0, 'max': 50})
     class Meta:
         model = Thing
         fields = ['name', 'description', 'quantity']
         widgets = { 'description':forms.Textarea(), 'quantity':forms.NumberInput() }
+
+    def clean(self):
+        super().clean()
