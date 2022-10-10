3f61281db7f7fb76c321c528bc1f25dc96ea820b
Abbas BinVakas <abbas.bin_vakas@kcl.ac.uk>
2022-10-10 19:31:59 +0100

    Exercise 1 attempt

--- things/forms.py
+++ things/forms.py
@@ -5,7 +5,11 @@ from .models import Thing
 
 # Create your forms here.
 
-class ThingForm(forms.Form):
-    name = forms.CharField(max_length=35, required=False)
-    description = forms.Textarea(max_length=120, required=False)
-    quantity = forms.NumberInput(attrs={'min':0, 'max': 50})
+class ThingForm(forms.ModelForm):
+    # name = forms.CharField(max_length=35, required=False)
+    # description = forms.Textarea(max_length=120, required=False)
+    # quantity = forms.NumberInput(attrs={'min':0, 'max': 50})
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = { 'description':forms.Textarea(), 'quantity':forms.NumberInput() }
